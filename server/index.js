12a1dbd7f99a6959836fff2052ec7113407985ad
AbuUsaid <usaidabu321@gmail.com>
2024-05-01 23:56:36 +0530

    create MySql db connection, send a get request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import mysql from 'mysql2';
+
+/*environment variable */
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const password = process.env.SQL_PASSWORD;
 
 const app = express();
 
-app.listen(
-  (8800,
-  () => {
-    console.log(`Connected to backend! `);
-  })
-);
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: password,
+  database: 'test',
+});
+
+app.get('/', (req, res) => {
+  res.json('hello this is the backend');
+});
+
+app.listen(8800, () => {
+  console.log(`Connected to backend! `);
+});
