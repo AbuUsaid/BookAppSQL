2a322eea9c75ccd3732326d6cb4410df251d9a13
AbuUsaid <usaidabu321@gmail.com>
2024-05-07 14:34:46 +0530

    create a post request

--- server/index.js
+++ server/index.js
@@ -17,10 +17,30 @@ const db = mysql.createConnection({
   database: 'test',
 });
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json('hello this is the backend');
 });
 
+app.get('/books', (req, res) => {
+  const q = 'SELECT * FROM books';
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post('/books', (req, res) => {
+  const q = 'INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)';
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json('Book has been created successfully');
+  });
+});
+
 app.listen(8800, () => {
-  console.log(`Connected to backend! `);
+  console.log(`Connected to backend! ${`http://localhost:8800/`} `);
 });
